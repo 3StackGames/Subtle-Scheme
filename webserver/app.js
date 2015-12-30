9e2b733c5b98e15cd8b44ea1fd65533e657fe886
arjun <sarodearjun57@gmail.com>
2015-12-30 03:43:46 -0600

    add cors headers on webserver

--- webserver/app.js
+++ webserver/app.js
@@ -30,6 +30,12 @@ app.set('view engine', 'jade')
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization')
+  next()
+})
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
